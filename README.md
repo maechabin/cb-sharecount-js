242e7e1178682ac56b5171dfddd31c05c7ed3b4c
maechabin <mail@chab.in>
2016-07-02 18:40:27 +0900

    v1.1.0

--- README.md
+++ README.md
@@ -1,55 +1,57 @@
-#jquery.cb-Share-Count.js
+#jquery.cbsharecount.js
 
-##＜概要＞
+## 概要
 
 任意のURL（複数可）のSNS上でのシェア数を非同期で取得して表示するためのjQueryプラグイン。
 
-##＜特徴＞
+## 特徴
 - 実装が簡単。プラグインを実行させたいjQueryオブジェクトに指定したセレクター要素のtitle属性に[シェア数を取得したいURL]を設定するだけ。**JavaScriptの実装はたったの1行**。
 - 複数のURLの値の取得ができるので、ブログ記事の一覧などでの使用に最適。
 - 非同期かつ並列処理で値を取得するので、ページの表示にもやさしい。
 - 値だけを取得するので、レイアウトもデザインも自由に行える。
-- はてなブックマーク登録数、Facebookいいね数の2サービスに対応
+- はてなブックマーク登録数、Facebookいいね数の2サービスに対応。
+- localStorageを使ったcache機能付き
 
+## 実装方法
 
-##＜実装方法＞
-
-###読み込み
+### 読み込み
 jQueryとjquery.cbsharecount.jsをページに読み込みます。
 ```html
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 <script src="jquery.cbsharecount.min.js"></script>
 ```
 
-###JavaScript
+### JavaScript
 JavaScriptの実装は以下の1行のみ！（複数のURLの値を取得する場合でも）
 ```html
 <script>
-$(セレクター).cbShareCount();
+$(document).ready(function () {
+  $(セレクター).cbShareCount();
+});
 </script>
 ```
 
-
-###HTML
+### HTML
 HTMLの実装は以下のルールさえ押さえておけば、あとは自由です。
 
-####URLの取得部分
-[ルール1]  
-jQueryオブジェクトに指定したセレクター要素のtitle属性に**[シェア数を取得したいURL]**を設定
+#### URLの取得部分
+[ルール1]
+jQueryオブジェクトに指定したセレクター要素のtitle属性に`[シェア数を取得したいURL]`を設定
+
 ```html
 <div class="[セレクター]" title="[シェア数を取得したいページのURL]">
-    [取得した値を表示させる箇所]
+  [取得した値を表示させる箇所]
 </div>
 ```
 
-####値の表示部分
+#### 値の表示部分
 [ルール2]  
-jQueryオブジェクトに指定したセレクター要素の子要素にそれぞれ以下の**[クラス属性]**を指定
+jQueryオブジェクトに指定したセレクター要素の子要素にそれぞれ以下の`[クラス属性]`を指定
 - **class="cb-hb"**　→　はてなブックマークの登録数表示用
 - **class="cb-fb"**　→　Facebookのいいね数表示用
 
 [ルール3]  
-値は上記のclass属性をつけた要素の子要素の**[span要素]**に表示される。
+値は上記のclass属性をつけた要素の子要素の`[span要素]`に表示される。
 ```html
 <div class="[セレクター]" title="[シェア数を取得したいページのURL]">
 	<!--子要素-->
@@ -57,9 +59,9 @@ jQueryオブジェクトに指定したセレクター要素の子要素にそ�
 </div>
 ```
 
-##＜実装例＞
+## 実装例
 
-###ulとliを使った実装例
+### ulとliを使った実装例
 ```html
 <ul class="[セレクター]" title="[シェア数を取得したいページのURL]">
     <li class="cb-hb">はてブ数<span></span></li>
@@ -67,7 +69,7 @@ jQueryオブジェクトに指定したセレクター要素の子要素にそ�
 </ul>
 ```
 
-###divとpを使った実装例
+### divとpを使った実装例
 ```html
 <div class="[セレクター]" title="[シェア数を取得したいページのURL]">
     <p class="cb-fb">Facebook<a href="#"><span></span></a></p>
@@ -75,7 +77,7 @@ jQueryオブジェクトに指定したセレクター要素の子要素にそ�
 </div>
 ```
 
-###複数のURLの値を取得する際の実装例（繰り返処理しても可）
+### 複数のURLの値を取得する際の実装例（繰り返処理しても可）
 ```html
 <div class="[セレクター（同じもの）]" title="[シェア数を取得したいページのURL1]">
     <p class="cb-fb">Facebook<a href="#"><span></span></a></p>
@@ -89,18 +91,21 @@ jQueryオブジェクトに指定したセレクター要素の子要素にそ�
 ```
 
 
-##＜デモ＞
+## デモ
 
 [http://jsdo.it/maechabin/mixw](http://jsdo.it/maechabin/mixw)
 
 私のブログにも実装済みです。  
 [http://mae.chab.in](http://mae.chab.in/)
 
-##＜ライセンス＞
+## ライセンス
 
 MIT license
 
-##＜アップデート情報＞
+## アップデート情報
+
+2016-07-01 v1.1.0
+- キャッシュ機能の追加（1日保存）
 
 2015-11-21 v1.0.3
- - TwitterのAPIの仕様変更により、Twitterのツイート数表示機能を停止
+- TwitterのAPIの仕様変更により、Twitterのツイート数表示機能を停止
--- src/app.js
+++ src/app.js
@@ -1,150 +1,125 @@
 /*!
- * jquery.cbsharecount.js v1.0.3
+ * jquery.cbsharecount.js v1.1.0
  * Auther @maechabin
  * Licensed under mit license
  * https://github.com/maechabin/jquery.cb-share-count.js
  */
 ;(function (factory) {
-
-  if (typeof module === "object" && typeof module.exports === "object") {
-
-    module.exports = factory(require("jquery"), window, document);
-
+  if (typeof module === 'object' && typeof module.exports === 'object') {
+    module.exports = factory(require('jquery'), window, document);
   } else {
-
     factory(jQuery, window, document);
-
   }
-
 } (function ($, window, document, undefined) {
-
-  "use strict";
+  'use strict';
 
   var Share = function (element, i) {
-
     this.element = element;
     this.$element = $(element);
-    this.site_url = "";
-    this.api_url = "";
-    this.param_name = "";
+    this.site_url = '';
+    this.api_url = '';
+    this.param_name = '';
     this.send_data = {};
     this.num = i;
-
   };
 
   Share.prototype.data = {
-/*
-    twitter: {
-      api_url: "http://urls.api.twitter.com/1/urls/count.json",
-      param_name: "url",
-      count: 0
-    },
-*/
     facebook: {
-      api_url: "http://graph.facebook.com/",
-      param_name: "id",
+      api_url: 'http://graph.facebook.com/',
+      param_name: 'id',
       count: 0
     },
-
     hatena: {
-      api_url: "http://api.b.st-hatena.com/entry.count",
-      param_name: "url",
+      api_url: 'http://api.b.st-hatena.com/entry.count',
+      param_name: 'url',
       count: 0
     }
-
   };
 
-  Share.prototype.get_count = function () {
-
+  Share.prototype.getCount = function getCount() {
     var d = new $.Deferred();
 
     $.ajax({
-
-      type: "get",
+      type: 'get',
       url: this.api_url,
       cache: true,
-      dataType: "jsonp",
+      dataType: 'jsonp',
       data: this.send_data,
       success: d.resolve,
       error: d.reject
-
     });
-
     return d.promise();
-
   };
 
-  Share.prototype.view = function (arg) {
-
+  Share.prototype.view = function view(arg) {
     var that = this;
-//    var tw = $(".cb-tw").eq(that.num).find("span");
-    var fb = $(".cb-fb").eq(that.num).find("span");
-    var hb = $(".cb-hb").eq(that.num).find("span");
-
     $(arg).each(function (i) {
-
       switch (i) {
-/*
-        case 0:
-          that.data.twitter.count = this[0].count || "";
-          break;
-*/
         case 0:
           that.data.facebook.count = this[0].shares || this[0].likes || 0;
           break;
-
         case 1:
-          that.data.hatena.count = this[0] || "";
+          that.data.hatena.count = this[0];
           break;
-
+        default:
+          break;
       }
-
     });
-
-//    tw.html(that.data.twitter.count);
-    fb.html(that.data.facebook.count);
-    hb.html(that.data.hatena.count);
-
+    that.save();
+    return that.render();
   };
 
-  Share.prototype.setup = function () {
+  Share.prototype.render = function render() {
+    var fb = $('.cb-fb').eq(this.num).find('span');
+    var hb = $('.cb-hb').eq(this.num).find('span');
+    fb.html(this.data.facebook.count);
+    hb.html(this.data.hatena.count);
+  };
 
+  Share.prototype.save = function save() {
+    localStorage.setItem('sc_' + this.site_url, JSON.stringify({
+      fbCount: this.data.facebook.count,
+      hbCount: this.data.hatena.count,
+      saveDate: new Date().getTime()
+    }));
+  };
+
+  Share.prototype.setup = function setup() {
     var that = this;
     var df = [];
 
     $.each(that.data, function (key, val) {
-
       that.api_url = val.api_url;
       that.send_data[val.param_name] = that.site_url;
-      df.push(that.get_count());
-
+      df.push(that.getCount());
     });
 
     $.when.apply($, df).done(function () {
-
-      that.view(arguments);
-
+      return that.view(arguments);
     });
-
   };
 
-  Share.prototype.init = function () {
+  Share.prototype.checkCache = function checkCache() {
+    var cache = JSON.parse(localStorage.getItem('sc_' + this.site_url)) || null;
+    var currentDate = new Date().getTime();
 
-    this.site_url = this.$element.attr("title");
-    this.setup();
+    if (cache && currentDate - cache.saveDate < 86400000) {
+      this.data.facebook.count = cache.fbCount;
+      this.data.hatena.count = cache.hbCount;
+      return this.render();
+    }
+    return this.setup();
+  };
 
+  Share.prototype.init = function init() {
+    this.site_url = this.$element.attr('title');
+    this.checkCache();
     return this;
-
   };
 
   $.fn.cbShareCount = function () {
-
     return this.each(function (i) {
-
       new Share(this, i).init();
-
     });
-
   };
-
 }));
